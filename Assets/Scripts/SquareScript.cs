93abfed7f618c2c7fee20f1568c93c900d29e70d
Shinjin Santhakumar <shinjinoliver@gmail.com>
2023-04-13 00:19:33 -0700

    fixed

--- Assets/Scripts/SquareScript.cs
+++ Assets/Scripts/SquareScript.cs
@@ -7,7 +7,7 @@ public class SquareScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("im the big dog");
+        Debug.Log("im the smol dog");
     }
 
     // Update is called once per frame
